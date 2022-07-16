319fa38a7e315e114c52caa5b593e603d534f390
thougtgeek <contact@thoughtgeek.com>
2022-07-16 10:07:35 +0000

    Use postgresql db

--- elements/settings.py
+++ elements/settings.py
@@ -77,12 +77,18 @@ WSGI_APPLICATION = 'elements.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('DB_NAME', 'postgres'),
+        'USER': env.str('DB_USER', 'postgres'),
+        'PASSWORD': env.str('DB_PASSWORD', 'postgres'),
+        'HOST':  env.str('DB_HOST', 'db'),
+        'PORT': env.str('DB_PORT', '5432'),
     }
 }
+
 
 
 # Password validation
